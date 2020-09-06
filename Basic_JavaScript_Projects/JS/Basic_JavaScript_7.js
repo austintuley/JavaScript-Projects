5f517e51340f331d9156a9dc171b45934ea70f00
Austin <austintuley@protonmail.com>
2020-09-06 17:53:26 -0600

    Push files # 7

--- Basic_JavaScript_Projects/JS/Basic_JavaScript_7.js
+++ Basic_JavaScript_Projects/JS/Basic_JavaScript_7.js
@@ -9,7 +9,8 @@ function addTwenty() {
 }
 
 addTen();
-addTwenty();
+document.write('<br>');
+//addTwenty();
 
 function getDate() {
 	if (new Date().getHours() < 18) {
@@ -26,3 +27,26 @@ function myFunction() {
 }
 
 myFunction();
+
+function eligibilityFunction() {
+	var seasons = document.getElementById('Seasons').value;
+	if (seasons < 2) {
+		document.getElementById('Response').innerHTML =
+			'You must complete 2 years of eligibility in college to qualify for the NFL draft.';
+	} else {
+		document.getElementById('Response').innerHTML = 'You may enter the NFL draft.';
+	}
+}
+
+function timeFunction() {
+	var time = new Date().getHours();
+	var reply;
+	if (time < 12 == time > 0) {
+		reply = 'It is the morning.';
+	} else if (time > 12 == time < 18) {
+		reply = 'It is the afternoon.';
+	} else {
+		reply = 'It is the evening.';
+	}
+	document.getElementById('Time_of_Day').innerHTML = reply;
+}
